1d503a928818026a76b1d4d271300bbf7318a3b4
rich1937 <ruchit.kapl@gmail.com>
2020-08-09 06:01:27 -0400

    second commit
    modified backend to route traffic from browser for /api to different routes in router. in router, based on subpath e.g. /:id and action e.g. put will perform certain actions on mongoseDB collections through module e.g. findByIdAndUpdate

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://appuser:mangoPLZ@cluster0.0khpf.mongodb.net/node-react-starter?retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true })
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT | 80
-app.get('/',(req,res)=>{
+/* app.get('/',(req,res)=>{
     res.send('hello')
-})
+}) */
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.use('/api', require('./routes/index'))
 
 app.listen(port)
 
