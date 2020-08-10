c4297713339ecdafc51fb8a039fbd2cb3442bbe1
rich1937 <ruchit.kapl@gmail.com>
2020-08-10 00:25:02 -0400

    dummy front end
    Created dummy front end with fields and buttons. Menu added, but not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      users:[],
+      id=0,
+      name:'',
+      email:'',
+      password:''
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="row">
+        <div className="col s6">
+          <form>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">person</i>
+              <input type="text" id="autocomplete-input" class="autocomplete" />
+              <label for="autocomplete-input">Name</label>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">mail</i>
+              <input type="email" id="autocomplete-input" class="autocomplete" />
+              <label for="autocomplete-input">Email</label>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">vpn_key</i>
+              <input type="password" id="autocomplete-input" class="autocomplete" />
+              <label for="autocomplete-input">Password</label>
+            </div>
+            <button class="btn waves-effect waves-light right" type="submit" name="action">Submit
+              <i class="material-icons right">send</i>
+            </button>
+          </form>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col s6">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Alvin</td>
+                <td>Eclair</td>
+                <td>$0.87</td>
+                <td>
+                  <button class="btn waves-effect waves-light" type="submit" name="action">
+                     <i class="material-icons">edit</i>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn waves-effect waves-light" type="submit" name="action">
+                    <i class="material-icons">delete</i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
